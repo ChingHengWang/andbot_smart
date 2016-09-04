c6f23465ade4ee215da6f3c48c0e31ca7aba8e97
ChingHengWang <qoogood1234@gmail.com>
2016-09-04 17:52:32 +0800

    mulit plot in imshow

--- python_plot/stkin_plot.py
+++ python_plot/stkin_plot.py
@@ -2,12 +2,17 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-img = mpimg.imread('stinkbug.png')
-imgplot = plt.imshow(img)
+img1 = mpimg.imread('stinkbug.png')
+plt.figure(1)
+imgplot1 = plt.imshow(img1)
 
-lum_img=img[:,:,0]
+img2 = mpimg.imread('pikachiu.png')
+plt.figure(2)
+imgplot2 = plt.imshow(img2)
+
+#lum_img=img[:,:,0]
 #plt.imshow(lum_img)
 #plt.imshow(lum_img, cmap="hot")
 
-imgplot = plt.imshow(lum_img)
+#imgplot = plt.imshow(lum_img)
 #imgplot.set_cmap('spectral')
